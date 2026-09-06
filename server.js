4d2787daab0cca34dbef49d7ccbafe0ec58af188
Habibi lal <lalhabibi91@gmail.com>
2026-09-06 10:37:49 +0530

    Add CORS support

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
+const cors = require('cors'); 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const DB_FILE = './users.json';
 
